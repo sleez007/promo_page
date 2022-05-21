313a011545441e72d6aa049d43b021271de2240c
Kingsley Etoka <etokakingsley@gmail.com>
2022-05-21 17:05:32 +0100

    desktop layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ButtonComponent } from './components/shared/button/button.component';
+import { BgComponent } from './components/bg/bg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    ButtonComponent,
+    BgComponent
   ],
   imports: [
     BrowserModule,
